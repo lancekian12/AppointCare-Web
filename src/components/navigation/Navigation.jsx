8835eade03323b852eda9e01f770844e2877c108
Lance Kian <lafa.flores.up@phinmaed.com>
2024-03-09 06:25:49 +0800

    updated

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom';
 import '../../css/Navigation.css';
 import logoImage from "/logo.png"
 import AppointCarelogo from "../../../public/AppointCareLogo.png"
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Navigation = ({ userData }) => {
     const [storedUserData, setStoredUserData] = useState(null);
